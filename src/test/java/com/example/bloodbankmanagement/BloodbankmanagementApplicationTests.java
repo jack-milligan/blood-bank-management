a2b5ea59dee4e1db17e69f1c9421131f8a7205b4
jack-milligan <jmill103@gmail.com>
2022-06-17 00:06:16 -0400

    Initial commit, uploading project structure to GitHub

---
+++ src/test/java/com/example/bloodbankmanagement/BloodbankmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloodbankmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodbankmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
